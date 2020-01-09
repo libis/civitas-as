c6ffc1f360ef9d936e8c0ff320b9d68250fa56fd
Joris <joris.lambrechts@kuleuven.be>
2020-01-09 15:02:20 +0100

    layout change fase II

--- modules/TilesBlock/src/Site/BlockLayout/TilesBlock.php
+++ modules/TilesBlock/src/Site/BlockLayout/TilesBlock.php
@@ -161,7 +161,7 @@ class TilesBlock extends AbstractBlockLayout
         $site = $block->page()->site();
 
         $query['site_id'] = $site->id();
-        $query['limit'] = $block->dataValue('limit', 12);
+        $query['limit'] = $block->dataValue('limit', 5);
 
         if (!isset($query['sort_by'])) {
             $query['sort_by'] = 'created';
